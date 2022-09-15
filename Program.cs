09514c17d231397d85bd2b1274da17f285bcee40
muccidavide <davidemucci0804@gmail.com>
2022-09-15 16:50:14 +0200

    Fix mediana and add console info

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "cioccolato", "gelato", "hamburger"};
+string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "cioccolato", "gelato", "hamburger", "carne", "fagioli"};
 
 // lunghezza classifica
-Console.WriteLine(cibiPreferiti.Length);
+Console.WriteLine("numero totale: " + cibiPreferiti.Length);
 
 // classifica
+Console.WriteLine("Classifica:");
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
     Console.WriteLine(i + " " + cibiPreferiti[i]);
 }
 
 // Cibo Top
-Console.WriteLine("Top: " + cibiPreferiti[0]);
+Console.WriteLine("Cibo primo posto: " + cibiPreferiti[0]);
 
 // Cibo notTop
 int posizioneUltimoCibo = cibiPreferiti.Length - 1;
-Console.WriteLine("Last: " + cibiPreferiti[posizioneUltimoCibo]);
+Console.WriteLine("Cibo ultimo posto: " + cibiPreferiti[posizioneUltimoCibo]);
 
 // BONUS: mediana
 double mediana = cibiPreferiti.Length % 2;
@@ -24,15 +25,14 @@ double mediana = cibiPreferiti.Length % 2;
 
 if(mediana == 0)
 {
-    int posizione1 = (cibiPreferiti.Length + 1)/ 2; // 3
-    int posizione2 = posizione1 + 1; // 4
+    int posizione1 = ((cibiPreferiti.Length + 1)/ 2) - 1; 
+    int posizione2 = posizione1 + 1; 
     
-    Console.WriteLine(cibiPreferiti[posizione1]);
-    Console.WriteLine(cibiPreferiti[posizione2]);
+    Console.WriteLine("Mediana: " + cibiPreferiti[posizione1] + " e " + cibiPreferiti[posizione2]);
 }
 else
 {
-    int posizione1 = (cibiPreferiti.Length - 1) / 2;
-    Console.WriteLine(cibiPreferiti[posizione1]);
+    int posizione1 = ((cibiPreferiti.Length + 2) / 2) - 1;
+    Console.WriteLine("Mediana: " + cibiPreferiti[posizione1]);
 
 }
