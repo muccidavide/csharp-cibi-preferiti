09b649824e9f0727e3dcd297ff2f02238e526510
muccidavide <davidemucci0804@gmail.com>
2022-09-15 15:52:11 +0200

    Complete Bonus

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "cioccolato", "gelato", "hamburger"};
+string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "cioccolato", "gelato", "hamburger", "carne"};
 
 Console.WriteLine(cibiPreferiti.Length);
 
@@ -13,3 +13,22 @@ Console.WriteLine(cibiPreferiti[0]);
 
 int posizioneUltimoCibo = cibiPreferiti.Length - 1;
 Console.WriteLine(cibiPreferiti[posizioneUltimoCibo]);
+
+double mediana = cibiPreferiti.Length % 2;
+Console.WriteLine(mediana);
+
+
+if(mediana == 0)
+{
+    int posizione1 = (cibiPreferiti.Length + 1)/ 2; // 3
+    int posizione2 = posizione1 + 1; // 4
+    
+    Console.WriteLine(cibiPreferiti[posizione1]);
+    Console.WriteLine(cibiPreferiti[posizione2]);
+}
+else
+{
+    int posizione1 = (cibiPreferiti.Length - 1) / 2;
+    Console.WriteLine(cibiPreferiti[posizione1]);
+
+}
