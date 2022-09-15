01eee1af40e2b206c20c60a63e188ffd67012512
muccidavide <davidemucci0804@gmail.com>
2022-09-15 15:20:42 +0200

    Complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-// prova commit
+string[] cibiPreferiti = { "pizza", "pasta", "lasagna", "cioccolato", "gelato", "hamburger"};
+
+Console.WriteLine(cibiPreferiti.Length);
+
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+
+Console.WriteLine(cibiPreferiti[0]);
+
+int posizioneUltimoCibo = cibiPreferiti.Length - 1;
+Console.WriteLine(cibiPreferiti[posizioneUltimoCibo]);
